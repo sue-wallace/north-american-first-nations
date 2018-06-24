37b155eda4bf2fbe012495db6cd69196c6053b31
mrmoleje <suzmwallace@gmail.com>
2018-06-24 16:18:30 +0100

    changed the marker from AddMarker to CircleMarker, as io wasn't recognising AddMarker

--- app.R
+++ app.R
@@ -17,46 +17,18 @@
 #shiny.rstudio.com
 
 #load libraries----
-
-library(readxl)
 library(leaflet)
-library(dplyr)
+#library(dplyr)
 library(htmltools)
 library(shiny)
 library(shinythemes)
-
-#read in data----
+library(readxl)
 
 d <-read_xlsx("Data/massacres.xlsx")
 
-
-#creating a map with one massare----
-
-leaflet() %>%
-  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=-82.7, lat=27.6, popup="Napituca Massacre")
-
-#add all massacres in data----
-
-
-#use the massacre data file to add all masscres in the data set to the map
-#using some basic html you can add some text and breaks to the pop up to make it a bit neater
-
-leaflet(data = d[1:25,]) %>% addTiles() %>%
-  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%  
-  addMarkers(lng=~longitude, lat=~latitude,
-             popup = ~paste(massacre_name, "<br>", "Date:", date, 
-                            "<br>", "Number of native casualties:", 
-                            native_casualties,"<a href=",d$web,">",d$web) 
-  )
-
-
-#adding the map to shiny----
-
-
 #create the UI
-
-ui <- {fluidPage(theme = shinytheme("slate"), titlePanel("Massacres in North America involving 
+ui <- {fluidPage(theme = shinytheme("slate"), titlePanel("Massacres in 
+                                                         North America involving 
                                                          First Nations Peoples: 1500-1700"), 
                  sidebarLayout(position = "right",
                                sidebarPanel(
@@ -66,30 +38,44 @@ ui <- {fluidPage(theme = shinytheme("slate"), titlePanel("Massacres in North Ame
                                ),
                                
                                mainPanel(
-                                 leafletOutput("mymap"))
+                                 leafletOutput("mymap")
+                               )
                  )
 )}
 
 
 server <- function(input, output) {
+  
+  
   react <- reactive({
     req(input$input1)
     df <- d[d$Tribe_name == input$input1,]
     df
-  })
+  }) 
+  
+  #This code  can be used to change the icon from a circle marker to a green leaf one.
+  
+  #greenLeafIcon <- makeIcon(
+   # iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+    #iconWidth = 38, iconHeight = 95,
+    #iconAnchorX = 22, iconAnchorY = 94,
+   # shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+   # shadowWidth = 50, shadowHeight = 64,
+   # shadowAnchorX = 4, shadowAnchorY = 62
+  #)
+  
   output$mymap <- renderLeaflet({ req(input$input1)
     
     leaflet(data = react()) %>% addTiles() %>% setView(lng = -100.94, lat = 38.94 , zoom = 3.5) %>% 
-      addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
-      addMarkers(lng = ~longitude, lat= ~latitude, 
-                 popup = paste(react()$massacre_name, "<br>", "Date:", react()$date, 
-                               "<br>", "Number of native casualties:", react()$native_casualties,
-                               "<b><a href"= react()$web))
-    
+      addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+      addCircleMarkers(lng = react()$longitude, lat= react()$latitude, #icon=greenLeafIcon, 
+                 popup = paste(react()$massacre_name, "<br>", "Date:",
+                               react()$date,
+                               "<br>", "Number of native casualties:",
+                               react()$native_casualties,
+                               "<b><a href"= react()$web)
+      ) 
   })
 }
 
-
 shinyApp(ui, server)
-
-
